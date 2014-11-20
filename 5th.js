c63dc12e9839a881a5d0b9d629e659d1d1abf379
unknown <dekel12@gmail.com>
2014-11-20 17:29:02 +0200

    done 5th

---
+++ 5th.js
@@ -0,0 +1,48 @@
+(function() {
+    function calculateEverything() {
+        var mr = $('.mr');
+        var sh = $('.sh');
+        var sum = $('.sum');
+        var totalMonthly = 0;
+        for (var i in mr) {
+            var value = Number(mr[i].value) * Number(sh[i].value);
+            if (isNaN(value))
+                value = '';
+            else
+                totalMonthly += value;
+            sum[i].value = value;
+        }
+        totalMonthly += isNaN(Number($('.manage').val())) ? 0 : Number($('.manage').val());
+        $('.totalMonthly').val(totalMonthly);
+        var len = Number($('#len').val()), optLen = Number($('#optionLen').val());
+        var optionTimes = Number($('#optionTimes').val()), optionRise = Number($('#optionRise').val());
+        var madad = Number($('#madad').val());
+        $('#table').html('');
+        if(isNaN(len) || isNaN(optLen) || isNaN(optionTimes) || isNaN(optionRise) || isNaN(madad))
+            return;
+        var table = $('#table');
+        var counter = 0;
+        for (i = 0;  i < len + optLen * optionTimes; i += 12, counter++){
+            var newPrice = totalMonthly;
+            if(i >= len){
+                newPrice = newPrice * Math.pow(1 + optionRise/100, 1 + Math.floor((i - len) / optLen));
+            }
+            newPrice = newPrice * Math.pow(1 + madad/100, counter + 1);
+            console.log(1 + madad/100);
+            var newRow = [
+                '<tr><td>שנה',
+                ' ',
+                counter + 1,
+                '</td><td>',
+                newPrice,
+                '</td><td>',
+                newPrice * 12,
+                '</td></tr>'
+            ].join('');
+            table.append(newRow);
+        }
+    }
+    var listening = $('.mr, .sh, .manage, #madad, #optionTimes, #optionLen, #len, #optionRise')
+    listening.keyup(calculateEverything);
+    listening.keydown(calculateEverything);
+})();
